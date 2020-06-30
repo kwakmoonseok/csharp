49c2ced928d08e318e17dd603c0e1042105961ce
kwakmoonseok <kms34510@naver.com>
2020-06-30 20:41:35 +0900

    Ex08
    8번 문제

---
+++ Ex08_Class_thisConstructor/Ex08_Class_thisConstructor/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex08_Class_thisConstructor
+{
+    class Program
+    {
+        class Class1
+        {
+            int a = 1, b = 0, c = 0;
+            public Class1() {
+
+            }
+
+            public Class1(int b)
+            {
+                this.b = b;
+            }
+
+            public Class1(int b, int c) : this(b)
+            {
+                this.c = c;
+            }
+
+            public void printField()
+            {
+                Console.WriteLine("a :" + a + " b : " + b + " c : " + c);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Class1 Class1 = new Class1();
+            Console.WriteLine("Class1() 호출");
+            Class1.printField();
+
+            Class1 Class2 = new Class1(2);
+            Console.WriteLine("Class1(2) 호출");
+            Class2.printField();
+
+            Class1 Class3 = new Class1(2, 3);
+            Console.WriteLine("Class1(2, 3) 호출");
+            Class3.printField();
+        }
+    }
+}
