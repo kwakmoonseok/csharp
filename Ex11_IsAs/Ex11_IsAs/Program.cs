7e6b167cb9d60a56865c2d5d177df7138a30dafd
kwakmoonseok <kms34510@naver.com>
2020-06-30 20:42:41 +0900

    Ex11
    11번 문제

---
+++ Ex11_IsAs/Ex11_IsAs/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex11_IsAs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            object[] objectArr = new object[] { 8, 9, 'c', null, 15.99, 745, true };
+            Console.WriteLine(SumDoubleOnly(objectArr));
+        }
+        static double SumDoubleOnly(object[] obArr)
+        {
+            double temp = 0;
+            foreach (object elements in obArr)
+            {
+                if (elements is double)
+                    temp += (double)elements;
+                
+            }
+            return temp;
+        }
+    }
+}
